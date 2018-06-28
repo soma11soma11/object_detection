345fb887e038ae39b7356dbc8417f12a01bce101
soma11soma11 <h2so4cos0@gmail.com>
2018-06-28 14:35:57 +0100

    created function that outputs detected objects in the form of csv with a column of the objects name and index of the image name. Also created a python script that can copy the corresponding images to the folder with the condition name

--- Object_detection_image.py
+++ Object_detection_image.py
@@ -34,7 +34,6 @@ from utils import visualization_utils as vis_util
 
 # Name of the directory containing the object detection module we're using
 MODEL_NAME = 'inference_graph'
-IMAGE_NAME = 'test1.jpg'
 
 # Grab path to current working directory
 CWD_PATH = os.getcwd()
@@ -47,7 +46,8 @@ PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
 PATH_TO_LABELS = os.path.join(CWD_PATH,'training','labelmap.pbtxt')
 
 # Path to image
-PATH_TO_IMAGE = os.path.join(CWD_PATH,IMAGE_NAME)
+IMAGE_FOLDER = 'target_images'
+PATH_TO_IMAGE = os.path.join(CWD_PATH,IMAGE_FOLDER)
 
 # Number of classes the object detector can identify
 NUM_CLASSES = 6
@@ -60,6 +60,10 @@ NUM_CLASSES = 6
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
+category_name = [categories[i]['name'] for i in range(len(categories))]
+empty_list = ["image"]
+category_name_header = empty_list + category_name
+
 
 # Load the Tensorflow model into memory.
 detection_graph = tf.Graph()
@@ -86,52 +90,77 @@ detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
 detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
 detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
 
-
 # Number of objects detected
 num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-
-# Load image using OpenCV and
-# expand image dimensions to have shape: [1, None, None, 3]
-# i.e. a single-column array, where each item in the column has the pixel RGB value
-image = cv2.imread(PATH_TO_IMAGE)
-image_expanded = np.expand_dims(image, axis=0)
-
-# Perform the actual detection by running the model with the image as input
-(boxes, scores, classes, num) = sess.run(
-    [detection_boxes, detection_scores, detection_classes, num_detections],
-    feed_dict={image_tensor: image_expanded})
-
 
 # Minimum score threshold for a box to be visualized  
 MIN_SCORE = 0.80
 
-# Export detectd categories to csv
-detected_index = [ n for n,i in enumerate(np.squeeze(scores)) if i>MIN_SCORE ]
-detected_class = itemgetter(*detected_index)(np.squeeze(classes).astype(np.int32))
-detected_category = [category_index[x]['name'] for x in detected_class]
-print(detected_category)
+# Export the detected objects to csv --- --- --- --- --- --- --- --- --- --- --- ---
 
-with open('detected_object.csv', 'w') as f:
-    writer = csv.writer(f, lineterminator='\n') 
-    writer.writerow(detected_category)     
+# Create function that outputs detected objects to csv
+def export_object_csv(filename):
+
+    # Get an image in the folder
+    image_directory = os.path.join(PATH_TO_IMAGE,filename)
+    image = cv2.imread(image_directory)
+    image_expanded = np.expand_dims(image, axis=0)
+
+    # Perform the actual detection by running the model with the image as input
+    (boxes, scores, classes, num) = sess.run(
+        [detection_boxes, detection_scores, detection_classes, num_detections],
+        feed_dict={image_tensor: image_expanded})
+
+    # Export detectd categories to csv
+    detected_index = [ n for n,i in enumerate(np.squeeze(scores)) if i>MIN_SCORE ]
+    detected_class = itemgetter(*detected_index)(np.squeeze(classes).astype(np.int32))
+    
+    # If the image contains only one object, need to convert int to tuple
+    if type(detected_class) is not tuple:
+        detected_class = (detected_class,)
+
+    detected_category = [category_index[x]['name'] for x in detected_class]
+    print(detected_category)
+
+    # Create a row that shows how many objets are in the image
+    objects_count = [detected_category.count(category_name[i]) for i in range(len(category_name))]
+    detected_objects_row = [filename] + objects_count
+
+    with open('detected_object.csv', 'a') as output_csvfile:
+        output_csvfile_writer = csv.writer(output_csvfile)
+        output_csvfile_writer.writerow(detected_objects_row)
+
+# Create a new empty detected_object.csv
+with open("detected_object.csv", "w") as output_csv:
+    writer = csv.writer(output_csv)
+    writer.writerow(category_name_header)
+
+# Run export_object_csv for each image in the folder
+for filename in os.listdir(PATH_TO_IMAGE):
+    if not filename.startswith('.') and os.path.isfile(os.path.join(PATH_TO_IMAGE, filename)):
+        export_object_csv(filename)
+
+
+
+# Draw the results on images --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
 # Draw the results of the detection (aka 'visulaize the results')
 
-vis_util.visualize_boxes_and_labels_on_image_array(
-    image,
-    np.squeeze(boxes),
-    np.squeeze(classes).astype(np.int32),
-    np.squeeze(scores),
-    category_index,
-    use_normalized_coordinates=True,
-    line_thickness=8,
-    min_score_thresh=MIN_SCORE)
+# vis_util.visualize_boxes_and_labels_on_image_array(
+#     image,
+#     np.squeeze(boxes),
+#     np.squeeze(classes).astype(np.int32),
+#     np.squeeze(scores),
+#     category_index,
+#     use_normalized_coordinates=True,
+#     line_thickness=8,
+#     min_score_thresh=MIN_SCORE)
 
 # All the results have been drawn on image. Now display the image.
-cv2.imshow('Object detector', image)
+# cv2.imshow('Object detector', image)
 
 # Press any key to close the image
-cv2.waitKey(0)
+# cv2.waitKey(0)
 
 # Clean up
-cv2.destroyAllWindows()
+# cv2.destroyAllWindows()
---
+++ get_image.py
@@ -0,0 +1,34 @@
+# Sort out (get) images based on how many objects are in there
+
+# Get the csv file
+import pandas as pd
+import os
+import shutil
+dataset = pd.read_csv("detected_object.csv")
+
+
+# Get an image that has at least one "jack"
+# Here, two conditions are specified: that image should have one "ace" and no "queen"
+# You can add more conditions if you need to 
+object_name1 = "jack"
+number_object1 = 1
+
+object_name2 = "queen"
+number_object2 = 0
+
+# Create a folder with the conditions name
+image_folder_name = "target_images/" + object_name1 + ">=" + str(number_object1) + "_" + object_name2 + "=" + str(number_object2)
+if not os.path.exists(image_folder_name):    
+    os.makedirs(image_folder_name)
+
+# Get the image name based on the criteria
+conditions = (dataset[object_name1] >= number_object1) & (dataset[object_name2] == number_object2)
+targeted_images_list = dataset["image"][conditions].tolist()
+
+# Copy corresponded images to the created folder
+for targeted_image in targeted_images_list:
+    src_dir = "target_images/" + targeted_image
+    shutil.copy(src_dir, image_folder_name)
+
+
+print(targeted_images_list)
